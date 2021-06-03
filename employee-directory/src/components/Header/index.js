49c426bcf0e22ed9e5f8aaa5e8a36ef64e0fcff7
Michael O'Connor <oconnor97@aol.com>
2021-06-03 13:54:25 -0400

    beginning components

---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div class="p-5 text-center bg-light">
+            <h1 class="mb-3">Heading</h1>
+            <h4 class="mb-3">Subheading</h4>
+        </div>
+    )
+}
+
+export default Header;
