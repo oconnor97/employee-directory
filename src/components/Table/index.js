3cf6664f007a24eb7c929f9aadde90f318c418ef
Michael O'Connor <oconnor97@aol.com>
2021-06-09 15:05:15 -0400

    creating a basic table layout component to create the user data

---
+++ src/components/Table/index.js
@@ -0,0 +1,26 @@
+const Table = () => {
+    return (
+        <div>
+            <table className="table table-hover table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Table
