73cccb76f25543fa225c1d36bfa91711980f9bce
Michael O'Connor <oconnor97@aol.com>
2021-06-09 15:06:58 -0400

    creating the layout for the header componenent

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
-import React from "react";
 
 
-function Header() {
+const Header = () => {
     return (
-        <div class="p-5 text-center bg-light">
-            <h1 class="mb-3">Heading</h1>
-            <h4 class="mb-3">Subheading</h4>
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Employee Directory</h1>
+            </div>
         </div>
     )
 }
 
-export default Header;
+export default Header
