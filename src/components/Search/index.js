4ae72c0af820bf7af3aa007f898519da56ff6484
Michael O'Connor <oconnor97@aol.com>
2021-06-09 15:04:46 -0400

    creating a component to hold the app search bar

---
+++ src/components/Search/index.js
@@ -0,0 +1,12 @@
+
+const search = () => {
+    return (
+        <div className="input-group">
+            <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
+                aria-describedby="search-addon" />
+            <button type="button" className="btn btn-outline-primary">search</button>
+        </div>
+    )
+}
+
+export default search
