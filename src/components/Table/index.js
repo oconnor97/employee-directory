18f4f34ee08eddfb4dcd8508ba7ed2847343cf95
Michael O'Connor <oconnor97@aol.com>
2021-06-10 11:22:57 -0400

    creating a function to make the api call and pass the data as props to the employeeData component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import EmployeeData from '../EmployeeData/index';
 import Search from '../Search/index';
 import API from '../../utils/API';
 
 
 const Table = () => {
+
+    const [employeeState, setEmployeeState] = useState([]);
+    const [filteredState, setFilteredState] = useState([]);
+
+    useEffect(() => {
+        loadEmployees();
+    }, []);
+
+
+    let allEmployees;
+
+    const loadEmployees = () => {
+        API.getEmployee()
+            .then((res) => {
+                allEmployees = res.data.results;
+                setFilteredState(allEmployees);
+                setEmployeeState(allEmployees)
+                console.log(allEmployees)
+            })
+            .catch(err => console.error(err));
+    }
+
+
     return (
         <div>
             <Search />
             <table className="table table-hover table-dark">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">DOB</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <EmployeeData />
+                    <EmployeeData employeeState={filteredState} />
                 </tbody>
             </table>
         </div>
