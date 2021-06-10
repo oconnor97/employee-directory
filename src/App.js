4f2fcb19df714e0877324f90c4f7fdc3aae528e2
Michael O'Connor <oconnor97@aol.com>
2021-06-10 13:01:24 -0400

    final revisions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header/index'
-
 import Table from './components/Table/index'
 
 
--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default {
     getEmployee: function () {
-        return axios.get("https://randomuser.me/api/?results=25")
+        return axios.get("https://randomuser.me/api/?results=50")
     }
 };
