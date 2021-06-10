3077a9ff64cebd5090b65d2a01d0c4cc49b1758b
Michael O'Connor <oconnor97@aol.com>
2021-06-10 11:23:16 -0400

    making basic edits

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
 
-        <div className="p-5 text-center bg-dark">
+        <div className="p-5 text-center bg-primary">
             <h1 className="mb-3 text-light">Employee Directory</h1>
             <h4 className="mb-3 text-light">Search to filter employees.</h4>
         </div>
