48016e12584f39f88e5db16b32b4edba4bfc60e5
Michael O'Connor <oconnor97@aol.com>
2021-06-10 11:22:00 -0400

    passing props to show the employee data on the page

--- src/components/EmployeeData/index.js
+++ src/components/EmployeeData/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const EmployeeData = () => {
+const EmployeeData = (props) => {
+    const employeeInfo = props.employeeState
     return (
-        <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr>
+        employeeInfo.map((employee) =>
+            <tr key={employee.id.value || employee.name.first}>
+                <th scope="row"><img alt={employee.name.first} src={employee.picture.thumbnail}></img></th>
+                <td>{employee.name.first} {employee.name.last}</td>
+                <td>{employee.phone}</td>
+                <td>{employee.email}</td>
+                <td>{employee.dob.date}</td>
+            </tr>
+        )
     )
 }
 
